65a5240ae824ca401fa2d4ee6e02f43c0e0c346c
Aruming <0430kar@naver.com>
2022-09-16 09:38:47 +0900

    세팅

---
+++ src/main/java/com/example/centOS/CentOsApplication.java
@@ -0,0 +1,13 @@
+package com.example.centOS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentOsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentOsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/centOS/CentOsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.centOS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentOsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
